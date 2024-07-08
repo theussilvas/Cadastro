489ced47b5eb200fe7355acdf4676178fad2cb6d
Matheus <silva.dantas2@escolar.ifrn.edu.br>
2024-07-07 23:23:39 -0300

    add requirements

--- Myapp/settings.py
+++ Myapp/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Cadastro',
     'bootstrap5',
-    'livereload',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
 
 ]
 
